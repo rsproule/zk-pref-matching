2be9395d10d7fc8ab6af051db42d03e6649586d4
Ryan Sproule <rsproule23@gmail.com>
2023-04-11 15:52:29 -0700

    proving of the matching algo on small preference sizes

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,24 +1,30 @@
+use matcher_core::Inputs;
 use methods::{PREF_MATCH_ELF, PREF_MATCH_ID};
-use risc0_zkvm::Prover;
+use risc0_zkvm::{Prover, serde::{to_vec, from_slice}};
 
 fn main() {
     // Make the prover.
     let mut prover =
         Prover::new(PREF_MATCH_ELF).expect("Prover should be constructed from valid ELF binary");
 
+    let men_preferences = vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 2, 0]];
+    let women_preferences = vec![vec![0, 1, 2], vec![2, 1, 0], vec![0, 2, 1]];
+    let inputs = Inputs {
+        preferences: (men_preferences, women_preferences),
+        pref_hashes: vec![],
+    };
     // TODO: Implement communication with the guest here
+    prover.add_input_u32_slice(&to_vec(&inputs).expect("input should be serializable"));
 
     // Run prover & generate receipt
     let receipt = prover.run().expect(
         "Code should be provable unless it had an error or exceeded the maximum cycle limit",
     );
 
-    // TODO: Implement code for transmitting or serializing the receipt for
-    // other parties to verify here
-
-    // Optional: Verify receipt to confirm that recipients will also be able to
-    // verify your receipt
     receipt.verify(&PREF_MATCH_ID).expect(
         "Code you have proven should successfully verify; did you specify the correct method ID?",
     );
+    let vec = receipt.journal;
+    let matches: Vec<usize> = from_slice(&vec).unwrap();
+    println!("Proven matches {matches:?}, in {:?} cycles", prover.cycles);
 }
--- methods/guest/src/bin/pref_match.rs
+++ methods/guest/src/bin/pref_match.rs
@@ -2,11 +2,17 @@
 
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std]  // std support is experimental
+// #![no_std]  // std support is experimental
+use matcher_core::{Inputs, gale_shapley};
+use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // TODO: Implement your guest code here
+    let inputs: Inputs = env::read();
+    // TODO: check the hash is correct  
+    let preferences = inputs.preferences;
+    let matches = gale_shapley(preferences.0, preferences.1);
     
+    env::commit(&matches);
 }
