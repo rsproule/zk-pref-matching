81e03e4ef3de00dc82d51ed1ba8e8716e0eeb4e4
Ryan Sproule <rsproule23@gmail.com>
2023-04-12 09:07:01 -0700

    update the readme with some notes on problems

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Privacy Preserving Stable Marriage Matching
 
 The stable marriage problem is a well understood problem that does blahj
+o
+
+
+## TODO
+
+So its fairly trivial to run the matching algorithm inside of risc zero. this is done and it works, using `65487` cycles. The issue is how do we get the preference arrays into risc zero without revealing them publicly or to a trusted 3rd party. I think this might not actually work cuz intuitively, there is shared public state in order to produce a proof. If there was some way to pass the hashes only for all of the preference arrays and verify that the computation was done on the correct data. This totally works for the example where there is a centralized trusted entity. The main problem with central entity is that they can leak data to other participants, or could be a player themselves.
--- host/src/main.rs
+++ host/src/main.rs
@@ -3,20 +3,16 @@ use methods::{PREF_MATCH_ELF, PREF_MATCH_ID};
 use risc0_zkvm::{Prover, serde::{to_vec, from_slice}};
 
 fn main() {
-    // Make the prover.
     let mut prover =
         Prover::new(PREF_MATCH_ELF).expect("Prover should be constructed from valid ELF binary");
-
-    let men_preferences = vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 2, 0]];
-    let women_preferences = vec![vec![0, 1, 2], vec![2, 1, 0], vec![0, 2, 1]];
+    let group_a_preferences = vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 2, 0]];
+    let group_b_preferences = vec![vec![0, 1, 2], vec![2, 1, 0], vec![0, 2, 1]];
     let inputs = Inputs {
-        preferences: (men_preferences, women_preferences),
+        preferences: (group_a_preferences, group_b_preferences),
         pref_hashes: vec![],
     };
-    // TODO: Implement communication with the guest here
     prover.add_input_u32_slice(&to_vec(&inputs).expect("input should be serializable"));
 
-    // Run prover & generate receipt
     let receipt = prover.run().expect(
         "Code should be provable unless it had an error or exceeded the maximum cycle limit",
     );
