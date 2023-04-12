3f05c8c484afaca6c9e867c3d6c14a624c794477
Ryan Sproule <rsproule23@gmail.com>
2023-04-12 14:53:31 -0700

    feat: improve stable marriage algorithm output format and preference array generation
    - Change the output of the stable marriage algorithm to include the number of cycles used
    - Comment out old preference arrays and generate new ones with a helper function

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,12 +1,17 @@
 use matcher_core::Inputs;
 use methods::{PREF_MATCH_ELF, PREF_MATCH_ID};
-use risc0_zkvm::{Prover, serde::{to_vec, from_slice}};
+use risc0_zkvm::{
+    serde::{from_slice, to_vec},
+    Prover,
+};
 
 fn main() {
     let mut prover =
         Prover::new(PREF_MATCH_ELF).expect("Prover should be constructed from valid ELF binary");
-    let group_a_preferences = vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 2, 0]];
-    let group_b_preferences = vec![vec![0, 1, 2], vec![2, 1, 0], vec![0, 2, 1]];
+    // let group_a_preferences = vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 2, 0]];
+    // let group_b_preferences = vec![vec![0, 1, 2], vec![2, 1, 0], vec![0, 2, 1]];
+    let group_a_preferences = gen_pref(100);
+    let group_b_preferences = gen_pref(100);
     let inputs = Inputs {
         preferences: (group_a_preferences, group_b_preferences),
         pref_hashes: vec![],
@@ -23,4 +28,16 @@ fn main() {
     let vec = receipt.journal;
     let matches: Vec<usize> = from_slice(&vec).unwrap();
     println!("Proven matches {matches:?}, in {:?} cycles", prover.cycles);
+}
+
+fn gen_pref(size: usize) -> Vec<Vec<usize>> {
+    let mut v = Vec::new();
+    for _ in 0..size {
+        let mut vv = Vec::new();
+        for j in 0..size {
+            vv.push(j)
+        }
+        v.push(vv);
+    }
+    v
 }
